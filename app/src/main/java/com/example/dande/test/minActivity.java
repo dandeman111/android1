b5ed62e665328625c529a303312ea487d0f09c9e
danny bergh <dandeman111@gmail.com>
2017-02-23 12:55:53 +0100

    ik heb een app waar ik + sommen kan doen en naar een leeg scherm kan gaan

--- app/src/main/java/com/example/dande/test/minActivity.java
+++ app/src/main/java/com/example/dande/test/minActivity.java
@@ -18,25 +18,23 @@ public class minActivity extends AppCompatActivity {
         setContentView(R.layout.activity_min);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        Button min = (Button) findViewById(R.id.btnMin);
-        //min.setOnClickListener(minListener1); //deze is fucked
+        Button btnMin = (Button) findViewById(R.id.btnGo);
+        btnMin.setOnClickListener(minListener1);
+
     }
 
     private View.OnClickListener minListener1 = new View.OnClickListener() {
+        @Override
         public void onClick(View v) {
 
-            //TextView tvEen = (TextView) findViewById(R.id.tvEen);
-            //TextView tvTwee = (TextView) findViewById(R.id.tvTwee);
-            //TextView tv1 = (TextView) findViewById(R.id.tvRes);
+            TextView tvEen = (TextView) findViewById(R.id.tvEen);
+            TextView tvTwee = (TextView) findViewById(R.id.tvTwee);
+            TextView tv1 = (TextView) findViewById(R.id.tvRes);
 
-
-
-
-            //long n1 = Long.parseLong(tvEen.getText().toString());
-            //long n2 = Long.parseLong(tvTwee.getText().toString());
-            //long res = n1 - n2;
-            //tv1.setText(Long.toString(res));
-
+            long n1 = Long.parseLong(tvEen.getText().toString());
+            long n2 = Long.parseLong(tvTwee.getText().toString());
+            long res = n1 - n2;
+            tv1.setText(Long.toString(res));
 
         }
     };
